aa5b18c6c4e98ad14ffbc697686baf5311f0a1c3
Keri Sen <keri.l.sen@gmail.com>
2024-01-06 09:34:52 -0800

    Installed inquirer and included dependencies

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // TODO: Include packages needed for this application
-// require inquirer
+const inquirer = require('inquirer');
 
 // TODO: Create an array of questions for user input
 const questions = [];
