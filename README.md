a7d3a82448f6a216ab40a568ede59ba5365b0a44
Keri Sen <keri.l.sen@gmail.com>
2024-01-26 17:29:09 -0800

    Link for the badge is generating but not displaying

--- README.md
+++ README.md
@@ -1,16 +1,46 @@
-## Just testing
-  # Description
+# Weather Dashboard
 
-  It solves a lot of world problems.
-  # Installation
+    ![GitHub License](https://img.shields.io/github/license/kerilsen/weather-dashboard)
 
-  Get a job.
-  # Usage
+  ## Description
 
-  Walk this way.
-  # Credits
+  find out the weather
 
-  Only the best.
-  # License
+  The GitHub repository is here: ![GitHub Repo](https://github.com/kerilsen/weather-dashboard)
 
-  MIT
+  The deployed link is here: ![Deployed page](https://kerilsen.github.io/weather-dashboard)
+
+## Table of Contents
+
+- [Installation](#installation)
+- [Usage](#usage)
+- [Contributing](#contributing)
+- [License](#license)
+- [Tests](#tests)
+- [Questions](#questions)
+
+  ## Installation
+
+  try it
+
+  ## Usage
+
+  no
+
+  ## Contributing
+
+  n/a
+
+  ## License
+
+  MIT License
+
+  ## Tests
+
+  n/a
+
+  ## Questions
+
+  If you have any questions about this project, please contact me at keri.l.sen@gmail.com.
+
+  My GitHub profile is at: ![GitHub Profile](https://github.com/kerilsen)
--- index.js
+++ index.js
@@ -2,21 +2,51 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 
-const generateREADME = ({ title, description, installation, usage, credits, license }) =>
-  `## ${title}
-  # Description\n
-  ${description}
-  # Installation\n
-  ${installation}
-  # Usage\n
-  ${usage}
-  # Credits\n
-  ${credits}
-  # License\n
-  ${license}`;
+const generateREADME = ({ username, repo, title, description, installation, usage, contributing, license, tests, email }) =>
+    `# ${title}\n
+    ![GitHub License](https://img.shields.io/github/license/${username}/${repo})\n
+  ## Description\n
+  ${description}\n
+  The GitHub repository is here: ![GitHub Repo](https://github.com/${username}/${repo})\n
+  The deployed link is here: ![Deployed page](https://${username}.github.io/${repo})\n
+## Table of Contents\n
+- [Installation](#installation)
+- [Usage](#usage)
+- [Contributing](#contributing)
+- [License](#license)
+- [Tests](#tests)
+- [Questions](#questions)\n
+  ## Installation\n
+  ${installation}\n
+  ## Usage\n
+  ${usage}\n
+  ## Contributing\n
+  ${contributing}\n
+  ## License\n
+  ${license}\n
+  ## Tests\n
+  ${tests}\n
+  ## Questions\n
+  If you have any questions about this project, please contact me at ${email}.\n
+  My GitHub profile is at: ![GitHub Profile](https://github.com/${username})\n`;
 
 // TODO: Create an array of questions for user input
 const questions = [
+    {
+        type: 'input',
+        name: 'username',
+        message: 'What is your GitHub username?'
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'What is your email address?'
+    },
+    {
+        type: 'input',
+        name: 'repo',
+        message: 'What is the GitHub repository name for this project (i.e. project-name)?'
+    },
     {
         type: 'input',
         name: 'title',
@@ -34,7 +64,13 @@ const questions = [
     {
         type: 'input',
         name: 'installation',
-        message: 'What are the steps required to install your project? Provide a step-by-step description of how to get the development environment running.\n'
+        message: 'What are the steps required to install your project?\nProvide a step-by-step description of how to get the development environment running.\nHit enter after each step.\n'
+    },
+    {
+        type: 'confirm',
+        name: 'askAgain',
+        message: 'Want to enter another step (just hit enter for YES)?',
+        default: true,
     },
     {
         type: 'input',
@@ -43,91 +79,57 @@ const questions = [
     },
     {
         type: 'input',
-        name: 'credits',
+        name: 'contributing',
         message: `List your collaborators, if any, with links to their GitHub profiles.\n
 If you used any third-party assets that require attribution, list the creators with links to their primary web presence in this section.\n
 If you followed tutorials, include links to those here as well.\n`
     },
-{
-type: 'input',
-name: 'license',
-message: 'The last section of a high-quality README file is the license. This lets other developers know what they can and cannot do with your project.'
-}
+    {
+        type: 'input',
+        name: 'links',
+        message: `Provide any links you would like to include in the credits in the following format: https://github.com/username\n`
+    },
+    {
+        type: 'list',
+        name: 'license',
+        message: `The last section of a high-quality README file is the license.\nThis lets other developers know what they can and cannot do with your project.\n
+The follow list is in order from highly protective to unconditional from https://choosealicense.com\n`,
+        choices: [
+            'GNU Affero General Public License v3.0',
+            'GNU General Public License v3.0',
+            'GNU Lesser General Public License v3.0',
+            'Mozilla Public License 2.0',
+            'Apache License 2.0',
+            'MIT License',
+            'Boost Software License 1.0',
+            'The Unlicense',
+        ]
+    },
+    {
+        type: 'input',
+        name: 'tests',
+        message: 'Go the extra mile and write tests for your application. Then provide examples on how to run them here.'
+    }
 ];
+
+function ask(answers) {
+    const list = [];
+    list.push(answers.installation)
+    if (answers.askAgain) {
+        ask();
+    } else {
+        return list;
+    }
+}
 
 inquirer
     .prompt(questions)
     .then((answers) => {
         const READMEcontent = generateREADME(answers);
         fs.writeFile('README.md', READMEcontent, (err) =>
-        err ? console.log(err) : console.log('Successfully created README.md!')
-      );
+            err ? console.log(err) : console.log('Successfully created README.md!')
+        );
     });
-
-// # <Your-Project-Title>
-
-// ## Description
-
-// Provide a short description explaining the what, why, and how of your project. Use the following questions as a guide:
-
-
-
-// ## Table of Contents (Optional)
-
-// If your README is long, add a table of contents to make it easy for users to find what they need.
-
-// - [Installation](#installation)
-// - [Usage](#usage)
-// - [Credits](#credits)
-// - [License](#license)
-
-// ## Installation
-
-// What are the steps required to install your project? Provide a step-by-step description of how to get the development environment running.
-
-// ## Usage
-
-// Provide instructions and examples for use. Include screenshots as needed.
-
-// To add a screenshot, create an `assets/images` folder in your repository and upload your screenshot to it. Then, using the relative filepath, add it to your README using the following syntax:
-
-//     ```md
-//     ![alt text](assets/images/screenshot.png)
-//     ```
-
-// ## Credits
-
-// List your collaborators, if any, with links to their GitHub profiles.
-
-// If you used any third-party assets that require attribution, list the creators with links to their primary web presence in this section.
-
-// If you followed tutorials, include links to those here as well.
-
-// ## License
-
-// The last section of a high-quality README file is the license. This lets other developers know what they can and cannot do with your project. If you need help choosing a license, refer to [https://choosealicense.com/](https://choosealicense.com/).
-
-// ---
-
-// 🏆 The previous sections are the bare minimum, and your project will ultimately determine the content of this document. You might also want to consider adding the following sections.
-
-// ## Badges
-
-// ![badmath](https://img.shields.io/github/languages/top/lernantino/badmath)
-
-// Badges aren't necessary, per se, but they demonstrate street cred. Badges let other developers know that you know what you're doing. Check out the badges hosted by [shields.io](https://shields.io/). You may not understand what they all represent now, but you will in time.
-
-// ## Features
-
-// If your project has a lot of features, list them here.
-
-// ## How to Contribute
-
-// If you created an application or package and would like other developers to contribute it, you can include guidelines for how to do so. The [Contributor Covenant](https://www.contributor-covenant.org/) is an industry standard, but you can always write your own if you'd prefer.
-
-// ## Tests
-
-// Go the extra mile and write tests for your application. Then provide examples on how to run them here.
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) { }
